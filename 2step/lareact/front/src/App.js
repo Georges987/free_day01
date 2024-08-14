9453f500b5b3ee882d819d455d5709f139e97681
= <=>
2024-08-14 08:54:53 +0100

    react mongodb

--- 2step/lareact/front/src/App.js
+++ 2step/lareact/front/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+async function uploadPost(data) {
+    await fetch('http://localhost:8000/api/posts/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+}
+
+async function getPosts() {
+    const response = await fetch('http://localhost:8000/api/posts/');
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = {
+        title: ''
+    };
+    var posts = getPosts();
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <input type="text" onClick={uploadPost(data)} value={data.title} />
+            </header>
+            {posts ?? posts.map((post) => <div key={post.id}>{post.title}</div>)}
+        </div>
+    );
 }
 
 export default App;
